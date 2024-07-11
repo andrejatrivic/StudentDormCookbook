e61bcf5c740747245f954f5ca833a2d0cfd9a868
andrejatrivic <andreja.trivic25@gmail.com>
2024-07-11 13:55:19 +0200

    suo

--- StudentDormCookbook.Server/obj/Debug/net6.0/StudentDormCookbook.Server.AssemblyInfo.cs
+++ StudentDormCookbook.Server/obj/Debug/net6.0/StudentDormCookbook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentDormCookbook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ad9f89f903c17f5f7eb9df43772060a5c9885b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentDormCookbook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentDormCookbook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
